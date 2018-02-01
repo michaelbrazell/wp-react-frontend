b8b040b306d6f089b29726b763bb79c3cb52a882
michaelbrazell <mike.murray@mathworks.com>
2018-02-01 14:53:28 -0500

    Initial commit with project structure

---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+  render() {
+    return (
+      <div className="navigation">
+        <ul>
+          <li>Navigation 1</li>
+          <li>Navigation 2</li>
+          <li>Navigation 3</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Nav;
