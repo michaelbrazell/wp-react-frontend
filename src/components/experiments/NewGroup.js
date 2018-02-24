04617d4bf9276dee1ff70d658efcff6791935a47
Mike Murray <michaelbrazell@gmail.com>
2018-02-24 15:50:35 -0500

    Experiments: Add some prettier styling

--- src/components/experiments/NewGroup.js
+++ src/components/experiments/NewGroup.js
@@ -16,12 +16,16 @@ class NewGroup extends Component {
     return (
       <div className="group-items">        
         {this.state.problems.map((problem, index) => ( 
-        	<div className="row" id={problem.id} key={problem.id + '-' + index}>
-            <div className="col">
-              {problem.id}. {problem.title}
-            </div>
-            <div className="col">
-              <button className="float-right" onClick={() => this.props.removeProblem(problem.id, problem.title)}>Remove</button>
+        	<div className="card mb-2" id={problem.id} key={problem.id + '-' + index}>
+            <div className="card-body">
+              <div className="row">
+                <div className="col">
+                  <p className="mb-0"> {problem.id}. {problem.title}</p>
+                </div>
+                <div className="col">
+                  <button className="float-right btn btn-sm btn-outline-primary" onClick={() => this.props.removeProblem(problem.id, problem.title)}>Remove</button>
+                </div>
+              </div>
             </div>
           </div>  	
         ))}
--- src/components/experiments/Problems.js
+++ src/components/experiments/Problems.js
@@ -12,12 +12,16 @@ class Problems extends Component {
     return (
       <div className="group-items">        
         {this.state.problems.map(problem => ( 
-        	<div className="row" id={problem.id} key={problem.id}>
-            <div className="col">
-              {problem.id}. {problem.title}
-            </div>
-            <div className="col">
-              <button className="float-right" onClick={() => this.props.addProblem(problem.id, problem.title)}>Add</button>
+        	<div className="card mb-2" id={problem.id} key={problem.id}>
+            <div className="card-body">
+              <div className="row">
+                <div className="col">
+                  <p className="mb-0">{problem.id}. {problem.title}</p>
+                </div>
+                <div className="col">
+                  <button className="float-right btn btn-sm btn-outline-primary" onClick={() => this.props.addProblem(problem.id, problem.title)}>Add</button>
+                </div>
+              </div>
             </div>
           </div>  	
         ))}
